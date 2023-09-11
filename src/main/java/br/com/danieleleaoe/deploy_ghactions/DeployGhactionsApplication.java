29d8b783598a5b5778e6c87880a6313aaee3c933
Daniele <daniele@MacBook-Pro-de-Daniele.local>
2023-09-11 19:51:59 -0300

    Create project

---
+++ src/main/java/br/com/danieleleaoe/deploy_ghactions/DeployGhactionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.danieleleaoe.deploy_ghactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployGhactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployGhactionsApplication.class, args);
+	}
+
+}
